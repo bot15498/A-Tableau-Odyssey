9d0e1cfa19ba7d1e24e4d7086a10c8aee0846540
AAYF <samuel.hawaii@gmail.com>
2020-04-05 06:27:18 -0500

    angry

---
+++ Boogie-Woogie/Assets/AngryBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngryBehavior : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    public GameObject Player;
+    bool foundPlayer;
+    PlayerDetected pd;
+    bool playerdetection;
+    public GameObject pissed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        pd = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayerDetected>();
+        foundPlayer = false;
+        pissed.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        playerdetection = pd.playerDetected;
+
+
+        if (playerdetection == true)
+        {
+            foundPlayer = true;
+        }
+
+        
+        if (foundPlayer == true)
+        {
+            pissed.SetActive(true);
+            var position = Vector3.MoveTowards(transform.position, new Vector3(Player.transform.position.x, transform.position.y, transform.position.z), speed * Time.deltaTime);
+            rb.MovePosition(position);
+        }
+
+
+    }
+}
---
+++ Boogie-Woogie/Assets/PlayerDetected.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetected : MonoBehaviour
+{
+    public bool playerDetected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDetected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void detectPlayer()
+    {
+        playerDetected = true;
+    }
+}
