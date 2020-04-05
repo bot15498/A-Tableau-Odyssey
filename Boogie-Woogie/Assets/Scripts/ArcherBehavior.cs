3a7ffb24ffdd46615d651805b1d395c0175d829f
skyler <konata.izumi20@gmail.com>
2020-04-05 07:29:23 -0400

    make archer.

---
+++ Boogie-Woogie/Assets/Scripts/ArcherBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherBehavior : MonoBehaviour
+{
+	public GameObject arrow;
+	public float fireRate = 1f;
+	public float fireAngle = 45f;
+	public float force = 100f;
+
+	private GameObject player;
+	private float currTime = 0f;
+
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    void Update()
+    {
+		if(currTime > fireRate)
+		{
+			GameObject spawnedArrow = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, fireAngle));
+			spawnedArrow.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 1f));
+			currTime = 0f;
+		}
+		currTime += Time.deltaTime;
+
+	}
+}
---
+++ Boogie-Woogie/Assets/Scripts/Arrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+	public int damage = 1;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		Collider2D actual = collision.GetContact(0).collider;
+		if (collision.gameObject.tag == "Player" && actual.gameObject.GetComponent<YellowBlock>() == null)
+		{
+			collision.gameObject.GetComponentInParent<PlayerStats>().TakeDamage(damage);
+		}
+		Destroy(gameObject);
+	}
+}
