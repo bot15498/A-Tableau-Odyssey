ced8b45e37913412abb6a2d64c768c8ccdd9e8df
AAYF <samuel.hawaii@gmail.com>
2020-04-05 09:27:51 -0500

    fwdads

---
+++ Boogie-Woogie/Assets/LaserBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehavior : MonoBehaviour
+{
+    public float speed;
+    public int damage = 1;
+    Rigidbody2D rb;
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D actual = collision.GetContact(0).collider;
+        if (collision.gameObject.tag == "Player" && actual.gameObject.GetComponent<YellowBlock>() == null)
+        {
+            collision.gameObject.GetComponentInParent<PlayerStats>().TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
---
+++ Boogie-Woogie/Assets/LookAtPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    private Transform player;
+    bool fightstarted;
+    public float firerate;
+    private float timer;
+    public GameObject laser;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        fightstarted = false;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vectorToTarget = player.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotateSpeed);
+
+        if (fightstarted == true)
+        {
+            timer += 1 * Time.deltaTime;
+            if(timer >= firerate)
+            {
+                Debug.Log("adfasdf");
+                Instantiate(laser, transform.position, transform.rotation);
+                timer = 0;
+            }
+        }
+    }
+
+    public void startFight()
+    {
+        fightstarted = true;
+    }
+}
