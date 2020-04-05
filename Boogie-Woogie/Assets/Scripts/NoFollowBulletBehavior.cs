391af879050bb66421ca0fbb2d2b594d55973f68
skyler <konata.izumi20@gmail.com>
2020-04-05 06:05:27 -0400

    scene changes

---
+++ Boogie-Woogie/Assets/Scripts/NoFollowBulletBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoFollowBulletBehavior : MonoBehaviour
+{
+	public float speed = 4f;
+	public int damage = 1;
+
+	private Rigidbody2D rb2d;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.velocity = transform.up * speed;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Collider2D actual = collision.GetContact(0).collider;
+		if (collision.gameObject.tag == "Player" && actual.gameObject.GetComponent<YellowBlock>() == null)
+		{
+			collision.gameObject.GetComponentInParent<PlayerStats>().TakeDamage(damage);
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Boogie-Woogie/Assets/Scripts/UnlockSector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockSector : MonoBehaviour
+{
+	public GameObject YellowSector;
+	private PersistantData data;
+
+    void Start()
+    {
+		if (data == null)
+		{
+			data = FindObjectOfType<PersistantData>();
+		}
+		YellowSector.SetActive(data.unlockYellow);
+	}
+
+	void Update()
+    {
+        if(data == null)
+		{
+			data = FindObjectOfType<PersistantData>();
+			YellowSector.SetActive(data.unlockYellow);
+		}
+    }
+}
