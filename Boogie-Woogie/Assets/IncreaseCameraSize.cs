f9e42abee18e17799d7ff55f4fdfcd8f5c12ea42
AAYF <samuel.hawaii@gmail.com>
2020-04-05 11:28:22 -0500

    asdf

---
+++ Boogie-Woogie/Assets/IncreaseCameraSize.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseCameraSize : MonoBehaviour
+{
+    Camera maincamera;
+    public float IncreaseRate;
+    public float maxCameraSize;
+    bool wasdf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincamera = Camera.main;
+        wasdf = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wasdf == true)
+        {
+            if (maincamera.orthographicSize <= maxCameraSize)
+            {
+                maincamera.orthographicSize += IncreaseRate * Time.deltaTime;
+            }
+        }
+
+        
+
+    }
+
+    public void oiuahydfoalifhdg()
+    {
+        wasdf = true;
+    }
+
+}
