381176e59a2078a9e5f21b721fff9ad4cd198745
AAYF <samuel.hawaii@gmail.com>
2020-04-04 20:08:50 -0500

    aaa

---
+++ Boogie-Woogie/Assets/Scripts/RotateInPlace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateInPlace : MonoBehaviour
+{
+    public float speed;
+    public Vector3 zAxis = new Vector3(0, 0, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(zAxis * speed);
+    }
+}
