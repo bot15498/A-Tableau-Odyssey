6bf7cb972518d50623a5fe70bff49bb010481edf
skyler <konata.izumi20@gmail.com>
2020-04-05 10:09:10 -0400

    menu and transition stuff.

---
+++ Boogie-Woogie/Assets/Scripts/BlockUnlockTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockUnlockTrigger : MonoBehaviour
+{
+	public bool unlockYellow = false;
+	public bool unlockRed = false;
+
+	private PersistantData pd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		pd = FindObjectOfType<PersistantData>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pd == null)
+		{
+			pd = FindObjectOfType<PersistantData>();
+		}
+    }
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(unlockYellow)
+		{
+			pd.ActivateYellow();
+		}
+		if (unlockRed)
+		{
+			pd.ActivateRed();
+		}
+	}
+}
---
+++ Boogie-Woogie/Assets/Scripts/RevealExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealExit : MonoBehaviour
+{
+	public GameObject exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+		exit.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	public void ActivateExit()
+	{
+		exit.SetActive(true);
+	}
+}
