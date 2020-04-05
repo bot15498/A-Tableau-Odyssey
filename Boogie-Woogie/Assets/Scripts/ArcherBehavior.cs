79787592dcab68d71a8d7c78e72f39307045c013
skyler <konata.izumi20@gmail.com>
2020-04-05 08:57:21 -0400

    archer hell.

--- Boogie-Woogie/Assets/Scripts/ArcherBehavior.cs
+++ Boogie-Woogie/Assets/Scripts/ArcherBehavior.cs
@@ -8,24 +8,38 @@ public class ArcherBehavior : MonoBehaviour
 	public float fireRate = 1f;
 	public float fireAngle = 45f;
 	public float force = 100f;
+	public bool fire = false;
 
 	private GameObject player;
 	private float currTime = 0f;
+	public PlayerDetected pd;
 
-    void Start()
-    {
+	void Start()
+	{
 		player = GameObject.FindGameObjectWithTag("Player");
+		pd = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayerDetected>();
 	}
 
-    void Update()
-    {
-		if(currTime > fireRate)
+	void Update()
+	{
+		if (pd.playerDetected)
 		{
-			GameObject spawnedArrow = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, fireAngle));
-			spawnedArrow.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Sin(Mathf.Deg2Rad * fireAngle), Mathf.Cos(Mathf.Deg2Rad * fireAngle)) * force);
-			currTime = 0f;
+			fire = true;
 		}
-		currTime += Time.deltaTime;
+	}
 
+	private void FixedUpdate()
+	{
+		if(fire)
+		{
+			if (currTime > fireRate)
+			{
+				Debug.Log(currTime);
+				GameObject spawnedArrow = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, fireAngle));
+				spawnedArrow.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Mathf.Sin(Mathf.Deg2Rad * fireAngle), Mathf.Cos(Mathf.Deg2Rad * fireAngle)) * force);
+				currTime = 0f;
+			}
+			currTime += Time.deltaTime;
+		}
 	}
 }
--- Boogie-Woogie/Assets/Scripts/Arrow.cs
+++ Boogie-Woogie/Assets/Scripts/Arrow.cs
@@ -16,9 +16,14 @@ public class Arrow : MonoBehaviour
 	}
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(rb2d == null)
+		{
+			rb2d = GetComponent<Rigidbody2D>();
+		}
+		float angle = Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x) * Mathf.Rad2Deg;
+		rb2d.MoveRotation(angle - 90);
     }
 
 	public void OnCollisionEnter2D(Collision2D collision)
