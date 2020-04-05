c34bd6936a48b5189f9dbb74d63003ed77f3ce80
skyler <konata.izumi20@gmail.com>
2020-04-05 00:24:44 -0400

    Add crafting bench sprite.

---
+++ Boogie-Woogie/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+	public int damage = 1;
+	//public float knockBackForce = 500f;
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		Collider2D actual = collision.GetContact(0).collider;
+		if (collision.gameObject.tag == "Player" && actual.gameObject.GetComponent<BlueBlock>() == null)
+		{
+			collision.gameObject.GetComponentInParent<PlayerStats>().TakeDamage(damage);
+			Vector2 moveDirection = collision.gameObject.transform.position - transform.position;
+			//collision.gameObject.GetComponentInParent<Rigidbody2D>().AddForce(Vector2.left * knockBackForce);
+		}
+	}
+}
---
+++ Boogie-Woogie/Assets/Scripts/PersistantData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantData : MonoBehaviour
+{
+	public bool unlockYellow = false;
+	public bool unlockRed = false;
+	public WeaponBuilder builder;
+    void Start()
+    {
+		DontDestroyOnLoad(this.gameObject);
+		if (builder == null)
+		{
+			builder = FindObjectOfType<WeaponBuilder>();
+			builder.YellowButtons.SetActive(false);
+			builder.RedButtons.SetActive(false);
+		}
+    }
+
+    void Update()
+    {
+		if (builder == null)
+		{
+			builder = FindObjectOfType<WeaponBuilder>();
+		}
+	}
+
+	public void ActivateYellow()
+	{
+		unlockYellow = true;
+		builder.YellowButtons.SetActive(true);
+	}
+
+	public void ActivateRed()
+	{
+		unlockRed = true;
+		builder.RedButtons.SetActive(true);
+	}
+}
